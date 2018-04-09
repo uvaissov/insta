ce85729187eb8cc6ca288130407453f19da41eea
Serik Uvaissov <suvaissov@networkenergy.net>
2018-04-09 10:16:42 +0600

    init

---
+++ src/test/java/kz/astana/uvaissov/insta/InstaApplicationTests.java
@@ -0,0 +1,16 @@
+package kz.astana.uvaissov.insta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InstaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

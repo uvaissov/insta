afff6f84793cad971c8a5d90f24fe05b279e54cd
Serik Uvaissov <suvaissov@networkenergy.net>
2018-08-15 12:55:30 +0600

    design start coding

--- src/main/resources/static/js/project/controller/DesignCtrl.js
+++ src/main/resources/static/js/project/controller/DesignCtrl.js
@@ -2,7 +2,12 @@ app.controller('DesignCtrl', DesignCtrl);
 function DesignCtrl () {
   var me = this; 
   me._contextPath = _contextPath+"cabinet/container/";
+  me.type='material';
+  me.changeType =function(type){
+	  me.type=type;
+  }
   
   
-
+  $("#custom1").spectrum({ color: "rgb(87, 168, 203)",showAlpha: true,chooseText: "Выбрать",cancelText: "Отмена" ,replacerClassName: 'color-input-swatch'});
+  $("#custom2").spectrum({ color: "rgb(202, 155, 155)",showAlpha: true,chooseText: "Выбрать",cancelText: "Отмена", replacerClassName: 'color-input-swatch'});
 };

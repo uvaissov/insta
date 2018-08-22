a41a789dd5e27e50abdeb4d90eecdff53c9ea5e5
Serik Uvaissov <suvaissov@networkenergy.net>
2018-08-22 11:17:50 +0600

    design final stage

--- src/main/resources/static/js/project/controller/DesignCtrl.js
+++ src/main/resources/static/js/project/controller/DesignCtrl.js
@@ -1,109 +1,136 @@
 app.controller('DesignCtrl', DesignCtrl);
-function DesignCtrl ($http,$window,$cookies) {
-  var me = this; 
-  me._contextPath = _contextPath+"cabinet/container/";
-  me.type='material';
-  me.backgroundList = backItemsMaterial ;
-  me.changeType =function(type){
-	  me.type=type;
-  }
-  
-  me.init = { mainColor:"rgb(87, 168, 203)" , backgroundColor:"rgb(202, 155, 155)" };
-  me.mainColor = me.init.mainColor;
-  me.backgroundColor = me.init.backgroundColor;
-  
-  
-  me.rowClassBack = function(div,background){
+function DesignCtrl($http, $window, $cookies) {
+	var me = this;
+	me._contextPath = _contextPath + "cabinet/container/";
+	me.type = 'material';
+	me.backgroundList = backItemsMaterial;
+	me.backgroundListC = backItemsCasual;
+	me.changeType = function(type) {
+		me.type = type;
+		me.slickerClass= me.type == 'material' ? '.slicker-material' : '.slicker-casual'
+		me.slickGoTo();
+		if(me.type == 'material')
+			me.setColorBackground();
+	}
+
+	me.init = {
+		mainColor : "rgb(87, 168, 203)",
+		backgroundColor : "rgb(202, 155, 155)"
+	};
+	me.mainColor = me.init.mainColor;
+	me.backgroundColor = me.init.backgroundColor;
+
+	me.rowClassBack = function(div, background) {
 		var str = background.name;
-		if(background.active===true){
-			str+=' selected';
+		if (background.active === true) {
+			str += ' selected';
 		}
 		return str;
 	};
-  
-	me.selectBackground=function(item){
-		angular.forEach(me.backgroundList, function(value) {
-			if(value.active===true){
-				value.active=false;
+
+	me.selectBackground = function(item) {
+		angular.forEach(me.type == 'material' ? me.backgroundList
+				: me.backgroundListC, function(value) {
+			if (value.active === true) {
+				value.active = false;
 			}
 		});
-		item.active=true;
+		item.active = true;
 	};
-  
-  
-$(document).ready(function() {
-	  //spectrum
-	  $("#custom1").spectrum(
-			  { 
-				  color: me.backgroundColor,
-				  chooseText: "Выбрать",
-				  cancelText: "Отмена" ,
-				  replacerClassName: 'color-input-swatch',
-		            preferredFormat: "hex",
-		            clickoutFiresChange: true,
-		            move: function move(color) {
-		            	me.mainColor = color.toHexString();
-		            	setColorBackground();
-		            },
-		            change: function change(color) {
-		            	me.mainColor = color.toHexString();
-		            	setColorBackground();
-		            }
-					  
-			  });
-	  $("#custom2").spectrum(
-			  { 
-				  color: me.mainColor,
-				  chooseText: "Выбрать",
-				  cancelText: "Отмена" ,
-				  replacerClassName: 'color-input-swatch',
-		            preferredFormat: "hex",
-		            clickoutFiresChange: true,
-		            move: function move(color) {
-		            	me.backgroundColor = color.toHexString();
-		            	setColorBackground();
-		            },
-		            change: function change(color) {
-		            	me.backgroundColor = color.toHexString();
-		            	setColorBackground();
-		            }
-					  
-			  });
-	  //slicker
-	  var slickerVal = Math.floor($window.innerWidth/180);
-	  slickerVal=slickerVal<3?3:slickerVal>8?8:slickerVal;
-	  $(".slicker-class").show();
-	  $(".slicker-class").slick({
-		  infinite : true,
-		  slidesToShow : slickerVal,
-		  speed : 400,
-		  slidesToScroll : slickerVal
-	  });
-	  setColorBackground();
-	  angular.element($window).bind('resize', function(){
-	      slicker(Math.floor($window.innerWidth/180));
-	    });
-	  angular.forEach(me.backgroundList, function(value, index) {
-		  if (value.active === true) {
-			  $('.slicker-class').slick('slickGoTo', index);
-		  }
-	  });
-	  
-  	function setColorBackground(){
-  		$.each(me.backgroundList, function( index, value ) {
-  			var id = value.name+'_img';
-	  		var svg = value.body.replace('fill="#000"', 'fill="' +me.mainColor  + '" fill-opacity="' + 1 + '"').replace(/\"/g, '\'').replace(/\</g, '%3C').replace(/\>/g, '%3E').replace(/\&/g, '%26').replace(/\#/g, '%23');
-	   		var url = 'url("data:image/svg+xml,' + svg + '")';
-	   		$(document.getElementById(id)).css("backgroundImage",url);
-	   		$(document.getElementById(id)).css("backgroundColor",me.backgroundColor);
-  		});
-  		  		
-  	};
-  
-  function slicker(slickerVal){
-	  slickerVal=slickerVal<3?3:slickerVal>8?8:slickerVal;
-	  $('.slicker-class').slick('slickSetOption', {slidesToShow:slickerVal,slidesToScroll:slickerVal});
-  }
-});
-  
+	
+	me.setColorBackground = function() {
+		$.each(me.backgroundList, function(index, value) {
+			var id = value.name + '_img';
+			var svg = value.body.replace(
+					'fill="#000"',
+					'fill="' + me.mainColor + '" fill-opacity="'
+							+ 1 + '"').replace(/\"/g, '\'')
+					.replace(/\</g, '%3C').replace(/\>/g, '%3E')
+					.replace(/\&/g, '%26').replace(/\#/g, '%23');
+			var url = 'url("data:image/svg+xml,' + svg + '")';
+			$(document.getElementById(id)).css("backgroundImage",
+					url);
+			$(document.getElementById(id)).css("backgroundColor",
+					me.backgroundColor);
+		});
+	};
+	
+
+	$(document).ready(
+			function() {
+				//spectrum
+				$("#custom1").spectrum({
+					color : me.backgroundColor,
+					chooseText : "Выбрать",
+					cancelText : "Отмена",
+					replacerClassName : 'color-input-swatch',
+					preferredFormat : "hex",
+					clickoutFiresChange : true,
+					move : function move(color) {
+						me.mainColor = color.toHexString();
+						me.setColorBackground();
+					},
+					change : function change(color) {
+						me.mainColor = color.toHexString();
+						me.setColorBackground();
+					}
+
+				});
+				$("#custom2").spectrum({
+					color : me.mainColor,
+					chooseText : "Выбрать",
+					cancelText : "Отмена",
+					replacerClassName : 'color-input-swatch',
+					preferredFormat : "hex",
+					clickoutFiresChange : true,
+					move : function move(color) {
+						me.backgroundColor = color.toHexString();
+						me.setColorBackground();
+					},
+					change : function change(color) {
+						me.backgroundColor = color.toHexString();
+						me.setColorBackground();
+					}
+
+				});
+				//slicker
+				var slickerVal = Math.floor($window.innerWidth / 180);
+				slickerVal = slickerVal < 3 ? 3 : slickerVal > 8 ? 8
+						: slickerVal;
+				$(".slicker-class").show();
+				$(".slicker-class").slick({
+					infinite : true,
+					slidesToShow : slickerVal,
+					speed : 400,
+					slidesToScroll : slickerVal
+				});
+				angular.element($window).bind('resize', function() {
+					me.slicker(Math.floor($window.innerWidth / 180));
+				});
+				me.slickGoTo =function(){
+					var IsGoed = false;
+					angular.forEach(me.type == 'material' ? me.backgroundList
+							: me.backgroundListC, function(value, index) {
+						if (value.active === true) {
+							$(me.slickerClass).slick('slickGoTo', index);
+							IsGoed =true;
+						}
+					});
+					if(IsGoed===false){
+						$(me.slickerClass).slick('slickGoTo', 0);
+					}
+				};
+				me.slicker = function(slickerVal) {
+					slickerVal = slickerVal < 3 ? 3 : slickerVal > 8 ? 8
+							: slickerVal;
+					$(me.slickerClass).slick('slickSetOption', {
+						slidesToShow : slickerVal,
+						slidesToScroll : slickerVal
+					});
+				};
+				
+				me.changeType(me.type);
+
+			});
+
 };
--- src/main/resources/static/js/project/controller/PrimaryCtrl.js
+++ src/main/resources/static/js/project/controller/PrimaryCtrl.js
@@ -4,18 +4,10 @@ function PrimaryCtrl ($http,$window,$cookies) {
 	var $uploadCrop;
 	var primary = this;
 	var pointUrl = _contextPath+'cabinet/data/primary';
-	
-	primary.backgroundList = backItemsCasual ;
 	primary.message = null;
 	primary.saveInfo = function(info){
 		if(	angular.isDefined(info) &&
 	  		angular.isDefined(info.username)){
-			angular.forEach(primary.backgroundList, function(value) {
-				  if(value.active===true){
-					  info.background = value.name;
-				  }
-			});
-			
 			$http.post(pointUrl, info)
 			.then(
 				       function(response){
@@ -48,25 +40,6 @@ function PrimaryCtrl ($http,$window,$cookies) {
 		});
 	};
 	
-	primary.rowClassBack = function(background){
-		var str = background.name;
-		if(background.active===true){
-			str+=' selected';
-		}
-		return str;
-	};
-	primary.selectBackground=function(item){
-		angular.forEach(primary.backgroundList, function(value) {
-			if(value.active===true){
-				value.active=false;
-			}
-		});
-		item.active=true;
-	};
-	primary.changeBackGround = function(){
-		 $('#userBackground').click();
-		 e.preventDefault();
-	}
 	primary.get = function(){
 		$http.get(pointUrl, null)
 		.then(
@@ -75,9 +48,7 @@ function PrimaryCtrl ($http,$window,$cookies) {
 					var textarea1 = document.getElementById('inputDescription');
 					setTimeout(function(){
 						textarea1.style.cssText = 'height:auto; padding:0';
-					      // for box-sizing other than "content-box" use:
-					      // el.style.cssText = '-moz-box-sizing:content-box';
-						textarea1.style.cssText = 'height:' + textarea1.scrollHeight + 'px';
+					 	textarea1.style.cssText = 'height:' + textarea1.scrollHeight + 'px';
 					    },0);
 				}, 
 				function(response){
@@ -100,25 +71,6 @@ function PrimaryCtrl ($http,$window,$cookies) {
   
   
   $(document).ready(function() {
-	  
-	  //slicker
-	  var slickerVal = Math.floor($window.innerWidth/180);
-	  slickerVal=slickerVal<3?3:slickerVal>8?8:slickerVal;
-	  $(".slicker-class").show();
-	  $(".slicker-class").slick({
-		  infinite : true,
-		  slidesToShow : slickerVal,
-		  speed : 400,
-		  slidesToScroll : slickerVal
-	  });
-	  angular.element($window).bind('resize', function(){
-	      slicker(Math.floor($window.innerWidth/180));
-	    });
-	  angular.forEach(primary.backgroundList, function(value, index) {
-		  if (value.active === true) {
-			  $('.slicker-class').slick('slickGoTo', index);
-		  }
-	  });
 	  
   	//croppie
   	$(document.body).on("change", "#userImage", function() {
@@ -166,10 +118,6 @@ function PrimaryCtrl ($http,$window,$cookies) {
 	
   });
   
-  function slicker(slickerVal){
-	  slickerVal=slickerVal<3?3:slickerVal>8?8:slickerVal;
-	  $('.slicker-class').slick('slickSetOption', {slidesToShow:slickerVal,slidesToScroll:slickerVal});
-  }
   function readFile(input) {
 		if (input.files && input.files[0]) {
 			$("#preloader").show();
@@ -193,8 +141,6 @@ function PrimaryCtrl ($http,$window,$cookies) {
     var el = this;
     setTimeout(function(){
       el.style.cssText = 'height:auto; padding:0';
-      // for box-sizing other than "content-box" use:
-      // el.style.cssText = '-moz-box-sizing:content-box';
       el.style.cssText = 'height:' + el.scrollHeight + 'px';
     },0);
   }

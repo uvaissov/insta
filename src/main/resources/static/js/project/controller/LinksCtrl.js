b1e8173a28e8a65c6b44adebbcb3ef874edcc3ed
uvaissov <uvaissov@gmail.com>
2018-08-23 14:43:39 +0600

    Link design and sorting tools

--- src/main/resources/static/js/project/controller/LinksCtrl.js
+++ src/main/resources/static/js/project/controller/LinksCtrl.js
@@ -33,7 +33,74 @@ function LinksCtrl ($http) {
   // load info
   me.get();
 	  $(document).ready(function() {
-		
+		  var cols = document.querySelectorAll('#columns .column');
+		    [].forEach.call(cols, function (col) {
+		        col.addEventListener('dragstart', handleDragStart, false);
+		        col.addEventListener('dragenter', handleDragEnter, false)
+		        col.addEventListener('dragover', handleDragOver, false);
+		        col.addEventListener('dragleave', handleDragLeave, false);
+		        col.addEventListener('drop', handleDrop, false);
+		        col.addEventListener('dragend', handleDragEnd, false);
+		    });
+
+		    var dragSrcEl = null;
+		    function handleDragStart(e) {
+		        if (e.target.className.indexOf('column') > -1) {
+		            dragSrcEl = e.target;
+		            dragSrcEl.style.opacity = '0.4';
+		            var dt = e.dataTransfer;
+		            dt.effectAllowed = 'move';
+		            dt.setData('text', dragSrcEl.innerHTML);
+
+		            // customize drag image for one of the panels
+		            if (dt.setDragImage instanceof Function && e.target.innerHTML.indexOf('X') > -1) {
+		                var img = new Image();
+		                img.src = 'dragimage.jpg';
+		                dt.setDragImage(img, img.width / 2, img.height / 2);
+		            }
+		        }
+		    }
+		    function handleDragOver(e) {
+		        if (dragSrcEl) {
+		            e.preventDefault();
+		            e.dataTransfer.dropEffect = 'move';
+		        }
+		    }
+		    function handleDragEnter(e) {
+		        if (dragSrcEl) {
+		            e.target.classList.add('over');
+		        }
+		    }
+		    function handleDragLeave(e) {
+		        if (dragSrcEl) {
+		            e.target.classList.remove('over');
+		        }
+		    }
+		    function handleDragEnd(e) {
+		        dragSrcEl = null;
+		        [].forEach.call(cols, function (col) {
+		            col.style.opacity = '';
+		            col.classList.remove('over');
+		        });
+		    }
+		    function handleDrop(e) {
+		        if (dragSrcEl) {
+		            e.stopPropagation();
+		            e.stopImmediatePropagation();
+		            e.preventDefault();
+		            if (dragSrcEl != this) {
+		                dragSrcEl.innerHTML = e.target.innerHTML;
+		                this.innerHTML = e.dataTransfer.getData('text');
+		            }
+		        }
+		    }
 	});
   
+	  
+  
+	  
 };
+
+
+
+;
